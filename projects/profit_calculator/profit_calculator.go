4e612adab1fbcb68ef4a23b84de38af6e0f245fd
fatsheepsweig <fatsheep.sweig@gmail.com>
2026-05-03 01:56:57 -0400

    feat: :art: Split all logic into functions

--- projects/profit_calculator/profit_calculator.go
+++ projects/profit_calculator/profit_calculator.go
@@ -18,32 +18,39 @@ func calculateRatio(ebt, profit float64) float64 {
 	return ebt / profit
 }
 
+func calculateFinancials(revenue, expenses, taxRate float64) (ebt, profit, ratio float64) {
+	return calculateEBT(revenue, expenses), calculateProfit(ebt, taxRate), calculateRatio(ebt, profit)
+}
+
+func getUserInput(prompt string) float64 {
+	var userInput float64
+	fmt.Print(prompt)
+	fmt.Scan(&userInput)
+	return userInput
+}
+
+func displayFinanceReport(ebt, profit, ratio float64) {
+
+}
 func main() {
 	// Variable declarations
-	var revenue, expenses, taxRate float64
+	// var revenue, expenses, taxRate float64
 
-	// Prompt user for revenue
-	fmt.Print("Enter revenue: ")
-	// Read user input from stdin
-	fmt.Scan(&revenue)
-	// Prompt user for expenses
-	fmt.Print("Enter expenses: ")
-	// Read user input from stdin
-	fmt.Scan(&expenses)
-	// Prompt user for tax rate
-	fmt.Print("Enter tax rate: ")
-	// Read user input from stdin
-	fmt.Scan(&taxRate)
+	// revenue = getUserInput("Enter revenue: ")
+	// expenses = getUserInput("Enter expenses: ")
+	// taxRate = float64(getUserInput("Enter tax rate: "))
 
 	// Calculate earnings before tax (EBT)
-	ebt := calculateEBT(revenue, expenses)
+	// ebt := calculateEBT(revenue, expenses)
 	// Calculate earnings after tax (profit)
-	profit := calculateProfit(ebt, taxRate)
+	// profit := calculateProfit(ebt, taxRate)
 	// Calculate ratio (EBT / profit)
-	ratio := calculateRatio(ebt, profit)
+	// ratio := calculateRatio(ebt, profit)
+
+	displayFinanceReport(getUserInput("Enter revenue: "), getUserInput("Enter expenses: "), getUserInput("Enter tax rate: "))
 
 	// Output calculations
-	fmt.Printf("Earnings before tax (EBT): $%.2f\n", ebt)
-	fmt.Printf("Earnings after tax (profit): $%.2f\n", profit)
-	fmt.Printf("Ratio: %.4f\n", ratio)
+	// fmt.Printf("Earnings before tax (EBT): $%.2f\n", ebt)
+	// fmt.Printf("Earnings after tax (profit): $%.2f\n", profit)
+	// fmt.Printf("Ratio: %.4f\n", ratio)
 }
